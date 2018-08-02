519df00b7906a665284b25c4ad87a8f36c358466
JuanCamiloRios <42045074+JuanCamiloRios@users.noreply.github.com>
2018-08-02 15:04:06 -0500

    Initial commit

---
+++ demoviews/demoviews/View2.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace demoviews
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class View2 : ContentPage
+	{
+		public View2 ()
+		{
+			InitializeComponent ();
+		}
+
+        private void pickMusic_SelectedIndexChanged(object sender,System.EventArgs e)
+        {
+            var elementoSelecionado = pickMusic.SelectedItem.ToString();
+            DisplayAlert("Genero Mùsica", elementoSelecionado, "Aceptar");
+        }
+    }
+}
